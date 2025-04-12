5ca27191450c3a8dfa67cdab0c9570b5df6fbae7
MakarGeosing <woodkov2@gmail.com>
2025-04-12 11:26:38 +0800

    )

---
+++ src/main/java/com/game/Main.java
@@ -0,0 +1,26 @@
+package com.game;
+
+import com.game.controllers.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("regLog.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 333);
+        stage.setTitle("Авторизация");
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
